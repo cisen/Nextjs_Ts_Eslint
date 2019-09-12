7549f020d85421a9c04fe673b01a3035169a9b78
cisen <csacxc@gmail.com>
2019-09-12 11:15:52 +0800

    feat: add static server

---  next.publicRuntimeConfig.js
+++  next.publicRuntimeConfig.js
@@ -1,5 +1,6 @@
 const isProd = process.env.NODE_ENV === 'production';
-const prodAssetPrefix = '/Nextjs_Ts_Eslint';
+// const prodAssetPrefix = '/Nextjs_Ts_Eslint';
+const prodAssetPrefix = '';
 const serviceWorkerFilename = 'service-worker.js';
 
 module.exports = {
